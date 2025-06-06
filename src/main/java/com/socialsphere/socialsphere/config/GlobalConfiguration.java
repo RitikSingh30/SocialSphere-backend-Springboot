60891ca04dabd9b71ce331486d6be06cdde3e85b
ritiksingh <ritikraj.rr87@gmail.com>
2025-06-07 01:40:05 +0530

    Removed the redundant lines of code and fixed some error.

--- src/main/java/com/socialsphere/socialsphere/config/GlobalConfiguration.java
+++ src/main/java/com/socialsphere/socialsphere/config/GlobalConfiguration.java
@@ -1,6 +1,5 @@
 package com.socialsphere.socialsphere.config;
 
-import com.socialsphere.socialsphere.security.AuthTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,10 +7,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class GlobalConfiguration {
-    @Bean
-    public AuthTokenFilter authenticationJwtTokenFilter() {
-        return new AuthTokenFilter();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/com/socialsphere/socialsphere/payload/response/SignupResponseDto.java
+++ src/main/java/com/socialsphere/socialsphere/payload/response/SignupResponseDto.java
@@ -2,11 +2,13 @@ package com.socialsphere.socialsphere.payload.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignupResponseDto {
     private String message;
     private Boolean success;
