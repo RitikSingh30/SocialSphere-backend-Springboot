59c80a10d0bff13703360c20b6dd26375aa11756
ritiksingh <ritikraj.rr87@gmail.com>
2025-06-23 23:17:07 +0530

    Moved TTL minutes in application.properties file.

--- src/main/java/com/socialsphere/socialsphere/config/MongoTTLConfig.java
+++ src/main/java/com/socialsphere/socialsphere/config/MongoTTLConfig.java
@@ -2,6 +2,7 @@ package com.socialsphere.socialsphere.config;
 
 import com.socialsphere.socialsphere.entity.OtpEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.index.Index;
@@ -15,12 +16,16 @@ public class MongoTTLConfig {
 
     private final MongoTemplate mongoTemplate;
 
+    @Value("${otp.ttl.minutes}")
+    private long ttlMinutes;
+
     @PostConstruct
     public void initIndexes() {
         mongoTemplate.indexOps(OtpEntity.class).ensureIndex(
                 new Index()
                         .on("createdAt", org.springframework.data.domain.Sort.Direction.ASC)
-                        .expire(5, TimeUnit.MINUTES)
+                        .named("otp_createdAt_ttl_index")
+                        .expire(ttlMinutes, TimeUnit.MINUTES)
         );
     }
 }
